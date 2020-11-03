a4b89250601334198bb1acc5d9c46f72a72e16ba
simoneinverardi <71282425+simoneinverardi@users.noreply.github.com>
2020-11-03 09:12:39 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,8 +10,22 @@ function setup() {
   video = createCapture(VIDEO);
   video.size(320, 240);
   video.hide();
+  pixelDensity(1);
 }
 
 function draw() {
-  image(video, 0, 0);
+  background(51);
+
+  loadPixels();
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      let index = (x + y * width)*4;
+      pixels[index+0] = x;
+      pixels[index+1] = random(255);
+      pixels[index+2] = y;
+      pixels[index+3] = 255;
+
+  }
+}
+updatePixels();
 }
